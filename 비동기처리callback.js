480bb80537df2859937486d1d3dc77a5ee451487
MIN-Sunwoo <khk9141@naver.com>
2025-05-20 11:51:19 +0900

    비동기 처리 - callback:

---
+++ 비동기처리callback.js
@@ -0,0 +1,11 @@
+function findUser(id){
+    const user = {
+        id: id,
+        name: "User" + id,
+        email: id+ "@test.com",
+    }
+    return user
+}
+
+const user = findUser(1)
+console.log("user:", user)
