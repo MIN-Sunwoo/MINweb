ad58d1c222f161b399d791058dfe8b77d812a3b1
MIN-Sunwoo <khk9141@naver.com>
2025-05-22 15:52:58 +0900

    제너레이터5

---
+++ 제너레이터5.js
@@ -0,0 +1,12 @@
+function* gen() {
+    let ask1 = yield "2+2=?";
+    console.log(ask1);
+
+    let ask2 = yield "3*3=?"
+    console.log(ask2);
+}
+
+let generator = gen();
+console.log(generator.next().value);
+console.log(generator.next(4).value);
+console.log(generator.next(9).done);
